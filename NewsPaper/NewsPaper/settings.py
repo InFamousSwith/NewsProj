27ff45d49418df16f2c26592d2da71f2ebdde7b7
InFamousSwith <gitlin.boris191@yandex.ru>
2022-03-27 20:36:03 +0300

    asd

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -31,6 +31,12 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 # Application definition
 
+EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
+EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
+EMAIL_HOST_USER = 'gitlin.boris191'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
+EMAIL_HOST_PASSWORD = 'GiTlinBoRis02'  # пароль от почты
+EMAIL_USE_SSL = True
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
